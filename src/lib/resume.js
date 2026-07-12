604f4993c6a5d8cea50f5ec702c505e973a84c16
Carl Avidano <cavidano@gmail.com>
2026-07-12 13:26:01 -0400

    removed resume page

--- src/lib/resume.js
+++
@@ -1,8 +0,0 @@
-import { parse } from 'yaml';
-import resumeSource from '../content/data/resume.yaml?raw';
-
-export function getResume() {
-  const [resume] = parse(resumeSource);
-
-  return resume;
-}
